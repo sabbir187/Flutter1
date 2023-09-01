c52c57892c4e14e0e95d35fc1a96a52e14be27d9
md sabbir <mdsabbirahmed187@gmail.com>
2023-09-01 19:56:27 +0600

    navigation

--- app3/lib/main.dart
+++ app3/lib/main.dart
@@ -15,13 +15,66 @@ class myapp extends StatelessWidget{
 
 }
 class homeactivity extends StatelessWidget{
-  const homeactivity({super.key});
-
   @override
   Widget build(BuildContext context) {
 return Scaffold(
   appBar: AppBar(title: Text("MyApp"),),
+  body: Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: [
+      ElevatedButton(onPressed: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context)=>activity1()));
+      }, child: Text('Go Activity1')),
+      ElevatedButton(onPressed: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context)=>activity2()));
+      }, child: Text('Go Activity2'))
+    ],
+  )
 );
+  }
+
+}
+
+
+class activity1 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Activity1"),),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ElevatedButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>homeactivity()));
+            }, child: Text('Go Homeactivity')),
+            ElevatedButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>activity2()));
+            }, child: Text('Go Activity2'))
+          ],
+        )
+    );
+  }
+
+}
+
+
+class activity2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Activity2"),),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ElevatedButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>homeactivity()));
+            }, child: Text('Go Homeactivity')),
+            ElevatedButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>activity1()));
+            }, child: Text('Go Activity1'))
+          ],
+        )
+    );
   }
 
 }

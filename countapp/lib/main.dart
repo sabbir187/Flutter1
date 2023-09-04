076a7bf082ca01a105c0b8379cfc5da65cd95a67
md sabbir <mdsabbirahmed187@gmail.com>
2023-09-04 21:52:36 +0600

    stateful count appa

---
+++ countapp/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(myapp());
+}
+class myapp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+  return MaterialApp(
+    title: "My App",
+    home: Homepage(),
+  );
+  }
+
+}
+class Homepage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+   return HomepageUI();
+  }
+
+}
+class HomepageUI extends State<Homepage>{
+
+  int countingNumber=0;
+
+  @override
+  Widget build(BuildContext context) {
+return Scaffold(
+  appBar: AppBar(title: Text("Counter App"),),
+  body: Center(
+   child: Text(countingNumber.toString()),
+  ),
+    floatingActionButton: FloatingActionButton(
+    child: Icon(Icons.add_circle,color: Colors.red,),
+    onPressed: (){
+      setState(() {
+        countingNumber=countingNumber+1;
+      });
+     
+    },
+
+),
+);
+  }
+
+}
